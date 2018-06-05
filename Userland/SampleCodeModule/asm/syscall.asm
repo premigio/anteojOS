dc00369ce6e905d814c37a56e9245311c0e6d494
Pedro Remigio Pingarilho <premigio@itba.edu.ar>
2018-06-04 23:21:26 -0300

    ahora si

--- Userland/SampleCodeModule/asm/syscall.asm
+++ Userland/SampleCodeModule/asm/syscall.asm
@@ -9,19 +9,3 @@ syscall:
   mov rsp, rbp
   pop rbp
   ret
-
-;_div0:
-;    push rbp
-;    mov rbp, rsp
-;    int 0x00
-;    mov rsp, rbp
-;    pop rbp
-;    ret
-
-;_invalidOpcode:
-;    push rbp
-;    mov rbp, rsp
-;    int 0x06
-;    mov rsp, rbp
-;    pop rbp
-;    ret
